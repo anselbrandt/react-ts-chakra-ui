0f6f74a173b744af8525e187e7702c5d3171a523
anselbrandt <ansel@anselbrandt.com>
2020-12-21 20:52:31 -0500

    Add colorMode

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, useColorMode } from "@chakra-ui/react";
 import styles from "./App.module.css";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 function App() {
+  const { colorMode } = useColorMode();
+  const color = {
+    light: "nebula.500",
+    dark: "nebula.300",
+  };
   return (
     <Box>
       <Flex justifyContent="flex-end">
         <ColorModeSwitcher m={3} />
       </Flex>
       <Box className={styles.app}>
-        <Box>react-ts-chakra-ui</Box>
+        <Box color={color[colorMode]}>react-ts-chakra-ui</Box>
       </Box>
     </Box>
   );
